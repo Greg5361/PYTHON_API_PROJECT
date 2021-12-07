622f605592e73338036d240be9879cd9f300c0d1
Greg <gregory.m.andres@gmail.com>
2021-12-07 02:25:54 -0500

    API

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "3d3e05e8caa0c898271a5634267099e0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDHY98zuHnGXbU_6p6jtQ3t8NwSlGKuW_c"
